d8eee190025a1f72085bdc117a2b11fa0b36ddbd
iSushaant <kumar.su5hant@yahoo.com>
2023-01-06 12:38:13 +0530

    adding project to repository

---
+++ logic.py
@@ -0,0 +1,26 @@
+import random
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def getPassword(nr_letters, nr_numbers, nr_symbols):
+    passAlpha = random.choices(letters, k=nr_letters)
+    passNumerics = random.choices(numbers, k=nr_numbers)
+    passSymbols = random.choices(symbols, k=nr_symbols)
+
+    return passAlpha + passNumerics + passSymbols
+
+
+def ListTOString(str):
+    password = ''
+    for x in str:
+        password += x
+        continue
+    return password
---
+++ main.py
@@ -0,0 +1,20 @@
+#Password Generator Project
+from logic import ListTOString, getPassword
+import random
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+passList = getPassword(nr_letters, nr_numbers, nr_symbols)
+easy_password = ListTOString(passList)
+print(f"easy password (without randomized order): {easy_password}")
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+random.shuffle(passList)
+hard_password = ListTOString(passList)
+print(f"hard password (with randomised order): {hard_password}")
